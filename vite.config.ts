17e500bb8ff7b2bfdedc483d9271df1096670dac
Paz Lazar <pazer@gmx.com>
2023-04-30 20:53:40 +0300

    added My lists + analyzing priority

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,25 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks: {
-  //         lodash: ["lodash"],
-  //         "@mui/icons-material": ["@mui/icons-material"],
-  //         "@mui/material": ["@mui/material"],
-  //         "@mui/x-data-grid": ["@mui/x-data-grid"],
-  //         highcharts: ["highcharts"],
-  //         "highcharts-react-official": ["highcharts-react-official"],
-  //         luxon: ["luxon"],
-  //         "@fontsource/roboto": ["@fontsource/roboto"],
-  //         recoil: ["recoil"],
-  //         "the-new-css-reset": ["the-new-css-reset"],
-  //         "react-window": ["react-window"],
-  //         "@emotion/react": ["@emotion/react"],
-  //         "@emotion/styled": ["@emotion/styled"],
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ["lodash"],
+          "@mui/icons-material": ["@mui/icons-material"],
+          "@mui/material": ["@mui/material"],
+          "@mui/x-data-grid": ["@mui/x-data-grid"],
+          highcharts: ["highcharts"],
+          "highcharts-react-official": ["highcharts-react-official"],
+          luxon: ["luxon"],
+          "@fontsource/roboto": ["@fontsource/roboto"],
+          recoil: ["recoil"],
+          "the-new-css-reset": ["the-new-css-reset"],
+          "react-window": ["react-window"],
+          "@emotion/react": ["@emotion/react"],
+          "@emotion/styled": ["@emotion/styled"],
+        },
+      },
+    },
+  },
 });
