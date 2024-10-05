fd8cd671b3b8ee59f1c4a6ad195d699372f77ede
Ben Gedi <bengedi@gmail.com>
2024-10-05 18:13:00 +0300

    udpate

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import eslint from 'vite-plugin-eslint';
-// import mkcert from 'vite-plugin-mkcert';
+import mkcert from 'vite-plugin-mkcert';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint(), svgr(),
-    // mkcert(),
+    mkcert(),
   ],
-  // server: { https: true },
-  server: { https: false },
+  server: { https: true },
+  // server: { https: false },
 });
